8e9b93aa3cd19737f247b545ef766f8cc67cabc3
mahiro42 <mahiro4ds@gmail.com>
2020-05-05 03:10:06 +0900

    Fix type annotations in blogs/views.py

--- django_blog/blogs/views.py
+++ django_blog/blogs/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from django.http.request import HttpRequest
+from django.http import HttpRequest, HttpResponse
 
 from .models import Blog
 
 
-def index(req: HttpRequest) -> render:
+def index(req: HttpRequest) -> HttpResponse:
     blogs = Blog.objects.order_by("-created_at")
     return render(req, "blogs/index.html", {"blogs": blogs})
 
 
-def detail(req: HttpRequest, blog_id: int) -> render:
+def detail(req: HttpRequest, blog_id: int) -> HttpResponse:
     blog = Blog.objects.get(id=blog_id)
     return render(req, "blogs/detail.html", {"blog": blog})
